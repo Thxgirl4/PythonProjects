8703ffe73f58f248a7a07f49cc102fd13e7e3dbb
Ana Julia <anajuliasilva3304@gmail.com>
2025-02-14 23:11:08 -0300

    aprendendo operadores

---
+++ Operadores.py
@@ -0,0 +1,24 @@
+55 == 55 # numeros iguais
+2 != 3 # numeros diferentes
+5 < 9 # numero menor que
+5 > 3 # numero maior que
+5 <= 5 # menor ou igual que
+8 >= 7 # maior ou igual que
+
+# = (atribuição) - coloca o valor à direita na variavel da esquerda
+
+# operador and
+# true and true - true
+# true and false - false
+# false and true - false
+# false and false - false
+
+#operador or
+# true or true - true
+# true or false - true
+# false or true - true
+# false or false - false
+
+# operador not
+# not true - false
+# not false - true
--- Teste.py
+++ Teste.py
@@ -1,1 +1,13 @@
-print("Ola senhora e senhores")
+print("Ola querido")
+print("Qual seu nome?")
+
+name = input()
+
+print("Eh otimo lhe conhecer " + name)
+print('O tamanho do seu nome eh: ')
+print(len(name))
+
+print('Qual sua idade')
+
+idade = input()
+print('voce vai ter ' + str(int(idade) + 1) + ' em um ano')
